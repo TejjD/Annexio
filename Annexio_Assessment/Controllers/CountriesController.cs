1970b79d3c7989b9bb9c48cab83b7a02f16a2699
Tejas Dwarkaram <tejas.dwarkaram@gmail.com>
2022-06-09 19:17:26 +0200

    * added in server caching

--- Annexio_Assessment/Controllers/CountriesController.cs
+++ Annexio_Assessment/Controllers/CountriesController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Annexio_Assessment.Controllers;
 
@@ -6,21 +7,38 @@ namespace Annexio_Assessment.Controllers;
 [Route("[controller]")]
 public class CountriesController : ControllerBase
 {
-    private static readonly HttpClient client = new();
+    private static readonly HttpClient Client = new();
+    private const string CountryListCacheKey = "countryList";
+
+    private readonly IMemoryCache _cache;
     
     private readonly ILogger<CountriesController> _logger;
 
-    public CountriesController(ILogger<CountriesController> logger)
+    public CountriesController(ILogger<CountriesController> logger, IMemoryCache cache)
     {
         _logger = logger;
+        _cache = cache ?? throw new ArgumentNullException(nameof(cache));
     }
 
     [HttpGet(Name = "GetCountries")]
-    public async Task<string> Get()
+    public async Task<string?> Get()
     {
-        var stringTask = client.GetStringAsync("https://restcountries.com/v2/all");
-
-        var msg = await stringTask;
+        if (_cache.TryGetValue(CountryListCacheKey, out string? msg))
+        {
+            _logger.Log(LogLevel.Information, "Country list found in cache");
+        }
+        else
+        {
+            _logger.Log(LogLevel.Information, "Country list not found in cache. Fetching from database");
+            var stringTask = Client.GetStringAsync("https://restcountries.com/v2/all");
+            msg = await stringTask;
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromSeconds(60))
+                .SetAbsoluteExpiration(TimeSpan.FromSeconds(3600))
+                .SetPriority(CacheItemPriority.Normal)
+                .SetSize(1024);
+            _cache.Set(CountryListCacheKey, msg, cacheEntryOptions);
+        }
 
         return msg;
     }
